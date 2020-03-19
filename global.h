eda20b73806e75f170386874f33312927e479a9e
MechJM <matosjoaops@protonmail.com>
2020-03-19 23:24:03 +0000

    First section is nearly done

---
+++ global.h
@@ -0,0 +1,19 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+struct mods
+{
+    int all;
+    int bytes;
+    int block_size;
+    int count_links;
+    int dereference;
+    int separate_dirs;
+    int max_depth;
+}mods;
+
+
+char path[50];
+
+
+#endif
--- simpledu.c
+++ simpledu.c
@@ -5,38 +5,47 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
+#include "global.h"
 
-struct mods
-{
-    int all;
-    int bytes;
-    int block_size;
-    int count_links;
-    int dereference;
-    int separate_dirs;
-    int max_depth
-}mods;
 
-int main(int argc,char* argv[])
+int main(int argc,char* argv[]/*,char* envp[]*/)
 {
     if (argc < 2) fprintf(stderr,"Usage: simpledu -l [pathname] [-a] [-b] [-B size] [-L] [-S] [--max-depth=N]\n");
 
+    //Setting default struct values
+    mods.all = 0;
     mods.count_links = 1;
+    mods.block_size = 0;
+    mods.count_links = 0;
+    mods.dereference = 0;
+    mods.separate_dirs = 0;
+    mods.max_depth = 0;
 
-    for (int i = 0;argv[i] != NULL;i++)
+    for (int i = 1; argv[i] != NULL; i++)
     {
         if (strcmp(argv[i],"-a") == 0 || strcmp(argv[i],"--all") == 0) mods.all = 1;
-        if (strcmp(argv[i],"-b") == 0 || strcmp(argv[i],"--bytes") == 0) mods.bytes = 1;
-        if (strcmp(argv[i],"-B") == 0)
+        else if (strcmp(argv[i],"-b") == 0 || strcmp(argv[i],"--bytes") == 0) mods.bytes = 1;
+        else if (strcmp(argv[i],"-B") == 0) mods.block_size = atoi(argv[++i]);
+        else if (strstr(argv[i],"--block-size=") != NULL)
         {
-            i++;
-            mods.block_size = atoi(argv[i]);
+            size_t i2;
+            for (i2 = 0; i2 < strlen(argv[i]); i2++)
+            {
+                if (argv[i][i2] == '=') break; 
+            }
+            mods.block_size = atoi(&argv[i][++i2]);
         }
-        if (strstr(argv[i],"--block-size=") != NULL)
+        else if (strcmp(argv[i],"-L") == 0 || strcmp(argv[i],"--dereference") == 0) mods.dereference = 1;
+        else if (strcmp(argv[i],"-S") == 0 || strcmp(argv[i],"--separate-dirs") == 0) mods.separate_dirs = 1;
+        else if (strstr(argv[i],"--max-depth=") != NULL)
         {
-            char* eq_pos = strstr(argv[i],"=");
-            char* num_pos = eq_pos + sizeof(char);
-            mods.block_size = atoi(num_pos);
+            size_t i2;
+            for (i2 = 0; i2 < strlen(argv[i]); i2++)
+            {
+                if (argv[i][i2] == '=') break;
+            }
+            mods.max_depth = atoi(&argv[i][++i2]);
         }
+        else if (strcmp(argv[i],"-l") != 0 && strcmp(argv[i],"--count-links") != 0) strcpy(path,argv[i]);
     }
 }
