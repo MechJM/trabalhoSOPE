ff95c900251811956a50150a0a9b4e3c3c6130ae
MechJM <matosjoaops@protonmail.com>
2020-03-19 13:03:56 +0000

    Started working on first point

--- simpledu.c
+++ simpledu.c
@@ -4,35 +4,39 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct mods
+{
+    int all;
+    int bytes;
+    int block_size;
+    int count_links;
+    int dereference;
+    int separate_dirs;
+    int max_depth
+}mods;
 
 int main(int argc,char* argv[])
 {
     if (argc < 2) fprintf(stderr,"Usage: simpledu -l [pathname] [-a] [-b] [-B size] [-L] [-S] [--max-depth=N]\n");
 
-    DIR* dirp;
+    mods.count_links = 1;
 
-    long int size = 0;
-
-    if ((dirp = opendir(argv[1])) == NULL)
+    for (int i = 0;argv[i] != NULL;i++)
     {
-        perror(argv[1]);
-        exit(1);
-    } 
-
-    struct dirent *entry;
-    struct stat stat_buf;
-
-    while ((entry = readdir(dirp)) != NULL)
-    {
-        if (stat(entry->d_name,&stat_buf) < 0)
+        if (strcmp(argv[i],"-a") == 0 || strcmp(argv[i],"--all") == 0) mods.all = 1;
+        if (strcmp(argv[i],"-b") == 0 || strcmp(argv[i],"--bytes") == 0) mods.bytes = 1;
+        if (strcmp(argv[i],"-B") == 0)
         {
-            fprintf(stderr,"Couldn't get entry stats.\n");
-            exit(2);
+            i++;
+            mods.block_size = atoi(argv[i]);
         }
-        if (S_ISDIR(stat_buf.st_mode)) printf("%-25ld%12s\n",size,entry->d_name);
-        size += stat_buf.st_blocks/2;
-        
+        if (strstr(argv[i],"--block-size=") != NULL)
+        {
+            char* eq_pos = strstr(argv[i],"=");
+            char* num_pos = eq_pos + sizeof(char);
+            mods.block_size = atoi(num_pos);
+        }
     }
-    printf("Total: %ld\n",size);
-    exit(0);
 }
